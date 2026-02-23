20248b5f1e002563f14e79391d09d480a2889e23
hassanmubiru <hassanmubiru7@gmail.com>
2026-02-23 05:02:59 +0300

    modified:   android/build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,17 +19,6 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
-subprojects {
-    plugins.withId("com.android.library") {
-        extensions.configure<com.android.build.gradle.LibraryExtension> {
-            compileOptions {
-                sourceCompatibility = JavaVersion.VERSION_11
-                targetCompatibility = JavaVersion.VERSION_11
-            }
-        }
-    }
-}
-
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
