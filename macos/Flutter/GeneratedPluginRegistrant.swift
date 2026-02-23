70739d74ccf7f09426ea54a5fbdad5b0556019ab
hassanmubiru <hassanmubiru7@gmail.com>
2026-02-23 05:49:48 +0300

    Changes to be committed: 	new file:   .flutter-plugins 	modified:   macos/Flutter/GeneratedPluginRegistrant.swift 	modified:   pubspec.lock

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -15,6 +15,7 @@ import firebase_messaging
 import firebase_storage
 import geolocator_apple
 import iris_method_channel
+import path_provider_foundation
 import shared_preferences_foundation
 import sqflite_darwin
 
@@ -29,6 +30,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   IrisMethodChannelPlugin.register(with: registry.registrar(forPlugin: "IrisMethodChannelPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
