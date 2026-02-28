20929b8ce9d1c6b823e23571eeef84a924ed788e
hassanmubiru <hassanmubiru7@gmail.com>
2026-02-28 10:07:31 +0300

    modified:   android/app/build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
 
     defaultConfig {
         applicationId = "com.juice.juicedates"
-        minSdk        = 23
+        minSdk = flutter.minSdkVersion
         targetSdk     = flutter.targetSdkVersion
         versionCode   = flutter.versionCode
         versionName   = flutter.versionName
