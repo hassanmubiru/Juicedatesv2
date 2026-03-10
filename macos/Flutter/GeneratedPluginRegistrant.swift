24a1d3174050bf24c7cd82df2bd29cfb3940333f
hassanmubiru <hassanmubiru7@gmail.com>
2026-03-10 06:25:23 +0300

    Changes to be committed: 	modified:   linux/flutter/generated_plugin_registrant.cc 	modified:   linux/flutter/generated_plugins.cmake 	modified:   macos/Flutter/GeneratedPluginRegistrant.swift 	modified:   pubspec.lock 	modified:   pubspec.yaml 	modified:   windows/flutter/generated_plugin_registrant.cc 	modified:   windows/flutter/generated_plugins.cmake
    Changes not staged for commit:
    	modified:   lib/features/premium/settings_screen.dart
    	modified:   macos/Flutter/GeneratedPluginRegistrant.swift
    	modified:   windows/flutter/generated_plugin_registrant.cc
    	modified:   windows/flutter/generated_plugins.cmake

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,8 +10,10 @@ import file_selector_macos
 import firebase_auth
 import firebase_core
 import firebase_messaging
+import firebase_storage
 import geolocator_apple
 import path_provider_foundation
+import share_plus
 import shared_preferences_foundation
 import sqflite_darwin
 
@@ -21,8 +23,10 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
